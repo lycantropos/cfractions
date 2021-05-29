e943c42572a92ffd99f1c7b9e2356058017fb94d
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-29 13:48:55 +0300

    Complete test for 'Fraction' class initializer

--- tests/base_tests/fraction_tests/test_initialization.py
+++ tests/base_tests/fraction_tests/test_initialization.py
@@ -8,8 +8,15 @@ from . import strategies
 def test_basic(numerator: int, denominator: int) -> None:
     result = Fraction(numerator, denominator)
 
-    assert bool(numerator) is bool(result.numerator)
     assert (not numerator and not result.numerator
             or not numerator % result.numerator)
     assert (not numerator and not result.numerator
             or not denominator % result.denominator)
+
+
+@given(strategies.numerators, strategies.denominators)
+def test_properties(numerator: int, denominator: int) -> None:
+    result = Fraction(numerator, denominator)
+
+    assert bool(numerator) is bool(result.numerator)
+    assert numerator * result.denominator == result.numerator * denominator
