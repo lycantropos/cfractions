e1cf6f5f7d579ac0324140ee4d6b239b977ac931
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-29 23:44:18 +0300

    Add test for 'Fraction.__eq__' magic method

---
+++ tests/base_tests/fraction_tests/test_eq.py
@@ -0,0 +1,28 @@
+from hypothesis import given
+
+from cfractions.base import Fraction
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.fractions)
+def test_reflexivity(fraction: Fraction) -> None:
+    assert fraction == fraction
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_symmetry(first: Fraction, second: Fraction) -> None:
+    assert equivalence(first == second, second == first)
+
+
+@given(strategies.fractions, strategies.fractions, strategies.fractions)
+def test_transitivity(first: Fraction,
+                      second: Fraction,
+                      third: Fraction) -> None:
+    assert implication(first == second and second == third, first == third)
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_connection_with_inequality(first: Fraction, second: Fraction) -> None:
+    assert equivalence(not first == second, first != second)
