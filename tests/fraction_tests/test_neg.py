e6e0c87912270fc758fcf60b1e0f5633543debb2
Azat Ibrakov <azatibrakov@gmail.com>
2021-05-31 20:45:11 +0300

    Add test for 'Fraction.__neg__' magic method

---
+++ tests/fraction_tests/test_neg.py
@@ -0,0 +1,47 @@
+import sys
+
+from hypothesis import given
+
+from cfractions import Fraction
+from tests.utils import (equivalence,
+                         skip_reference_counter_test)
+from . import strategies
+
+
+@given(strategies.fractions)
+def test_basic(fraction: Fraction) -> None:
+    result = -fraction
+
+    assert isinstance(result, Fraction)
+
+
+@given(strategies.fractions)
+def test_involution(fraction: Fraction) -> None:
+    result = -fraction
+
+    assert fraction == -result
+
+
+@given(strategies.fractions)
+def test_fixed_point(fraction: Fraction) -> None:
+    result = -fraction
+
+    assert equivalence(fraction == result, not fraction)
+
+
+@given(strategies.fractions)
+def test_involution(fraction: Fraction) -> None:
+    result = -fraction
+
+    assert fraction == -result
+
+
+@skip_reference_counter_test
+@given(strategies.fractions)
+def test_reference_counter(fraction: Fraction) -> None:
+    value_refcount_before = sys.getrefcount(fraction)
+
+    result = -fraction
+
+    value_refcount_after = sys.getrefcount(fraction)
+    assert value_refcount_after == value_refcount_before
