d696079c1f20547fa942c693a847fccc7582d772
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-01 22:29:11 +0300

    Complete test for 'Fraction.__ge__' magic method

--- tests/fraction_tests/test_ge.py
+++ tests/fraction_tests/test_ge.py
@@ -1,8 +1,11 @@
+import sys
+
 from hypothesis import given
 
 from cfractions import Fraction
 from tests.utils import (equivalence,
-                         implication)
+                         implication,
+                         skip_reference_counter_test)
 from . import strategies
 
 
@@ -30,3 +33,17 @@ def test_equivalents(first: Fraction, second: Fraction) -> None:
     assert equivalence(result, second <= first)
     assert equivalence(result, first > second or first == second)
     assert equivalence(result, second < first or first == second)
+
+
+@skip_reference_counter_test
+@given(strategies.fractions, strategies.fractions)
+def test_reference_counter(first: Fraction, second: Fraction) -> None:
+    first_refcount_before = sys.getrefcount(first)
+    second_refcount_before = sys.getrefcount(second)
+
+    result = first >= second
+
+    first_refcount_after = sys.getrefcount(first)
+    second_refcount_after = sys.getrefcount(second)
+    assert first_refcount_after == first_refcount_before
+    assert second_refcount_after == second_refcount_before
