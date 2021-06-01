6dec69d0f04a7880d929334d5f7540f4e0f57593
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-01 13:50:39 +0300

    Add test for 'Fraction.__radd__' magic method

--- tests/fraction_tests/strategies.py
+++ tests/fraction_tests/strategies.py
@@ -11,6 +11,7 @@ finite_floats = strategies.floats(allow_infinity=False,
 infinite_floats = strategies.sampled_from([math.inf, -math.inf])
 nans = strategies.just(math.nan)
 integers = numerators = strategies.integers()
+finite_non_fraction_numbers = integers | finite_floats
 denominators = (strategies.integers(max_value=-1)
                 | strategies.integers(min_value=1))
 
