a3ebfdc0b91b6fa558cd104cae0784839f17c4da
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-01 16:18:00 +0300

    Complete test for 'Fraction.__rmul__' magic method

--- tests/fraction_tests/test_rmul.py
+++ tests/fraction_tests/test_rmul.py
@@ -7,7 +7,7 @@ from . import strategies
 
 
 @given(strategies.finite_non_fraction_numbers, strategies.fractions)
-def test_connection_with_add(first: Number, second: Fraction) -> None:
+def test_connection_with_mul(first: Number, second: Fraction) -> None:
     result = first * second
 
     assert result == second * first
