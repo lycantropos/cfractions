b5fbc11139cc4d867d83d597b32407c237f14475
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-01 13:55:33 +0300

    Add test for 'Fraction.__mul__' magic method

---
+++ tests/fraction_tests/test_mul.py
@@ -0,0 +1,63 @@
+import math
+import sys
+
+from hypothesis import given
+
+from cfractions import Fraction
+from tests.utils import (equivalence,
+                         skip_reference_counter_test)
+from . import strategies
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_basic(first: Fraction, second: Fraction) -> None:
+    result = first * second
+
+    assert isinstance(result, Fraction)
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_commutativity(first: Fraction, second: Fraction) -> None:
+    assert first * second == second * first
+
+
+@given(strategies.fractions, strategies.zero_fractions)
+def test_absorbing_element(first: Fraction, second: Fraction) -> None:
+    assert first * second == second == second * first
+
+
+@given(strategies.fractions, strategies.fractions, strategies.fractions)
+def test_associativity(first: Fraction,
+                       second: Fraction,
+                       third: Fraction) -> None:
+    assert (first * second) * third == first * (second * third)
+
+
+@given(strategies.fractions, strategies.integers)
+def test_integer_argument(first: Fraction, second: int) -> None:
+    result = first * second
+
+    assert result == first * Fraction(second)
+
+
+@given(strategies.fractions, strategies.floats)
+def test_float_argument(first: Fraction, second: float) -> None:
+    result = first * second
+
+    assert isinstance(result, float)
+    assert equivalence(math.isnan(result),
+                       math.isnan(second) or not first and math.isinf(second))
+
+
+@skip_reference_counter_test
+@given(strategies.fractions, strategies.fractions)
+def test_reference_counter(first: Fraction, second: Fraction) -> None:
+    first_refcount_before = sys.getrefcount(first)
+    second_refcount_before = sys.getrefcount(second)
+
+    result = first * second
+
+    first_refcount_after = sys.getrefcount(first)
+    second_refcount_after = sys.getrefcount(second)
+    assert first_refcount_after == first_refcount_before
+    assert second_refcount_after == second_refcount_before
