a9673a8a189a91480c2a103fde76ee0a8d8376dd
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-01 13:05:34 +0300

    Complete test for 'Fraction.__repr__' magic method

--- tests/fraction_tests/test_repr.py
+++ tests/fraction_tests/test_repr.py
@@ -1,7 +1,10 @@
+import sys
+
 from hypothesis import given
 
 import cfractions
 from cfractions import Fraction
+from tests.utils import skip_reference_counter_test
 from . import strategies
 
 
@@ -17,3 +20,14 @@ def test_round_trip(fraction: Fraction) -> None:
     result = repr(fraction)
 
     assert eval(result, vars(cfractions)) == fraction
+
+
+@skip_reference_counter_test
+@given(strategies.fractions)
+def test_reference_counter(fraction: Fraction) -> None:
+    fraction_refcount_before = sys.getrefcount(fraction)
+
+    result = repr(fraction)
+
+    value_refcount_after = sys.getrefcount(fraction)
+    assert value_refcount_after == fraction_refcount_before
