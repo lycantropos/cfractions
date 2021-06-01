9336f6dcad0961399e906ec518e22d453e6187bc
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-01 13:36:04 +0300

    Add test for 'Fraction.__add__' magic method

--- tests/fraction_tests/strategies.py
+++ tests/fraction_tests/strategies.py
@@ -4,11 +4,13 @@ from hypothesis import strategies
 
 from cfractions import Fraction
 
+floats = strategies.floats(allow_infinity=True,
+                           allow_nan=True)
 finite_floats = strategies.floats(allow_infinity=False,
                                   allow_nan=False)
 infinite_floats = strategies.sampled_from([math.inf, -math.inf])
 nans = strategies.just(math.nan)
-numerators = strategies.integers()
+integers = numerators = strategies.integers()
 denominators = (strategies.integers(max_value=-1)
                 | strategies.integers(min_value=1))
 
@@ -21,5 +23,6 @@ non_interned_numerators = non_interned_denominators = (
     (strategies.integers(max_value=-256)
      | strategies.integers(min_value=257)).filter(is_not_interned))
 zeros = strategies.just(0)
+zero_fractions = strategies.builds(Fraction)
 fractions = (strategies.builds(Fraction, numerators, denominators)
              | strategies.builds(Fraction, finite_floats))
---
+++ tests/fraction_tests/test_add.py
@@ -0,0 +1,67 @@
+import math
+import sys
+
+from hypothesis import given
+
+from cfractions import Fraction
+from tests.utils import (equivalence,
+                         implication,
+                         skip_reference_counter_test)
+from . import strategies
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_basic(first: Fraction, second: Fraction) -> None:
+    result = first + second
+
+    assert isinstance(result, Fraction)
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_commutativity(first: Fraction, second: Fraction) -> None:
+    assert first + second == second + first
+
+
+@given(strategies.fractions, strategies.zero_fractions)
+def test_neutral_element(first: Fraction, second: Fraction) -> None:
+    assert first + second == first == second + first
+
+
+@given(strategies.fractions, strategies.fractions, strategies.fractions)
+def test_associativity(first: Fraction,
+                       second: Fraction,
+                       third: Fraction) -> None:
+    assert (first + second) + third == first + (second + third)
+
+
+@given(strategies.fractions, strategies.integers)
+def test_integer_argument(first: Fraction, second: int) -> None:
+    result = first + second
+
+    assert result == first + Fraction(second)
+
+
+@given(strategies.fractions, strategies.floats)
+def test_float_argument(first: Fraction, second: float) -> None:
+    result = first + second
+
+    assert isinstance(result, float)
+    assert equivalence(math.isfinite(result), math.isfinite(second))
+    assert equivalence(math.isnan(result), math.isnan(second))
+    assert implication(math.isinf(second)
+                       or not first and not math.isnan(second),
+                       result == second)
+
+
+@skip_reference_counter_test
+@given(strategies.fractions, strategies.fractions)
+def test_reference_counter(first: Fraction, second: Fraction) -> None:
+    first_refcount_before = sys.getrefcount(first)
+    second_refcount_before = sys.getrefcount(second)
+
+    result = first + second
+
+    first_refcount_after = sys.getrefcount(first)
+    second_refcount_after = sys.getrefcount(second)
+    assert first_refcount_after == first_refcount_before
+    assert second_refcount_after == second_refcount_before
