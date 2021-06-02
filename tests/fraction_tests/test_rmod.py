6310c3a7ba94c449b392ce0d165cbd06a1e5bfd6
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-02 04:46:17 +0300

    Add test for 'Fraction.__rmod__' magic method

---
+++ tests/fraction_tests/test_rmod.py
@@ -0,0 +1,20 @@
+from numbers import Complex
+
+import pytest
+from hypothesis import given
+
+from cfractions import Fraction
+from . import strategies
+
+
+@given(strategies.integers, strategies.non_zero_fractions)
+def test_connection_with_truediv(first: Complex, second: Fraction) -> None:
+    result = first % second
+
+    assert result == Fraction(first) % second
+
+
+@given(strategies.non_fraction_numbers, strategies.zero_fractions)
+def test_zero_divisor(first: Complex, second: Fraction) -> None:
+    with pytest.raises(ZeroDivisionError):
+        first % second
