0a4b1a1ef67f0bd8ce959f268f8c9d16a45273f8
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-02 05:01:55 +0300

    Add test for 'Fraction.__trunc__' magic method

---
+++ tests/fraction_tests/test_trunc.py
@@ -0,0 +1,20 @@
+import math
+
+from hypothesis import given
+
+from cfractions import Fraction
+from . import strategies
+
+
+@given(strategies.fractions)
+def test_basic(fraction: Fraction) -> None:
+    result = math.trunc(fraction)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.fractions)
+def test_value(fraction: Fraction) -> None:
+    result = math.trunc(fraction)
+
+    assert abs(result - fraction) < 1
