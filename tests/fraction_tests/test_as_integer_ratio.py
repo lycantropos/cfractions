91607e7ae1e53b1ee6db289157879b3ecbef6dad
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-03 09:20:32 +0300

    Add test for 'Fraction.as_integer_ratio' method

---
+++ tests/fraction_tests/test_as_integer_ratio.py
@@ -0,0 +1,20 @@
+from hypothesis import given
+
+from cfractions import Fraction
+from . import strategies
+
+
+@given(strategies.fractions)
+def test_basic(fraction: Fraction) -> None:
+    result = fraction.as_integer_ratio()
+
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert all(isinstance(element, int) for element in result)
+
+
+@given(strategies.fractions)
+def test_round_trip(fraction: Fraction) -> None:
+    result = fraction.as_integer_ratio()
+
+    assert Fraction(*result).as_integer_ratio() == result
