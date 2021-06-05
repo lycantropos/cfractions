d5656330dab07c3468dd0f3487ca05ab125a80f6
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-06 00:51:08 +0300

    Complete test for 'Fraction.__rpow__' magic method

--- tests/fraction_tests/test_rpow.py
+++ tests/fraction_tests/test_rpow.py
@@ -9,7 +9,7 @@ from . import strategies
 
 @given(strategies.non_fractions_rationals,
        strategies.small_non_negative_integral_fractions)
-def test_connection_with_truediv(first: int, second: Fraction) -> None:
+def test_connection_with_pow(first: int, second: Fraction) -> None:
     result = first ** second
 
     assert result == Fraction(first) ** second
