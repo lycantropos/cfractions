dc2c587eed86454a550b274026f0754160b73643
Azat Ibrakov <azatibrakov@gmail.com>
2024-04-26 23:10:46 +0200

    Complete project metadata

--- setup.py
+++ setup.py
@@ -1,50 +1,43 @@
-import platform
-from pathlib import Path
+from __future__ import annotations
 
-from setuptools import (find_packages,
-                        setup)
+import sys
+import typing as t
 
-import cfractions
+from setuptools import find_packages, setup
 
 project_base_url = 'https://github.com/lycantropos/cfractions/'
-
-
-def read_file(path_string: str) -> str:
-    return Path(path_string).read_text(encoding='utf-8')
-
-
-parameters = dict(
-        name=cfractions.__name__,
-        packages=find_packages(exclude=('tests', 'tests.*')),
-        package_data={cfractions.__name__: ['py.typed', '__init__.pyi']},
-        version=cfractions.__version__,
-        description=cfractions.__doc__,
-        long_description=read_file('README.md'),
-        long_description_content_type='text/markdown',
-        author='Azat Ibrakov',
-        author_email='azatibrakov@gmail.com',
-        license='MIT License',
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'Programming Language :: Python :: 3.10',
-            'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy',
-        ],
-        url=project_base_url,
-        download_url=project_base_url + 'archive/master.zip',
-        python_requires='>=3.7'
+parameters: dict[str, t.Any] = dict(
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    url=project_base_url,
+    download_url=project_base_url + 'archive/master.zip',
 )
-if platform.python_implementation() == 'CPython':
+if sys.implementation.name == 'cpython':
     from glob import glob
 
     from setuptools import Extension
+    from setuptools.command.build_ext import build_ext
 
-    parameters.update(ext_modules=[Extension(cfractions.__name__ + '._'
-                                             + cfractions.__name__,
-                                             glob('src/*.c'))],
-                      zip_safe=False)
+    class BuildExt(build_ext):
+        def build_extensions(self) -> None:
+            compile_args = []
+            compiler_type = self.compiler.compiler_type
+            if compiler_type == 'unix':
+                compile_args.append('-Werror')
+                compile_args.append('-Wall')
+                compile_args.append('-Wextra')
+                if sys.version_info < (3, 12):
+                    # Python3.12 introduces conversion warnings
+                    compile_args.append('-Wconversion')
+            elif compiler_type == 'msvc':
+                compile_args.append('/WX')
+                compile_args.append('/W3')
+            for extension in self.extensions:
+                extension.extra_compile_args += compile_args
+            super().build_extensions()
+
+    parameters.update(
+        cmdclass={build_ext.__name__: BuildExt},
+        ext_modules=[Extension('cfractions._cfractions', glob('src/*.c'))],
+        zip_safe=False,
+    )
 setup(**parameters)
